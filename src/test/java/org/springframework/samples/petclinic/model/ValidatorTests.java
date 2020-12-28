0a6d26e1709823d78dd2d92f68e770ea64e4f85b
Joe Chavez <you@example.com>
2020-12-27 22:02:05 -0800

    break it joe

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("badboy");
+		person.setFirstName("break it joe");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
